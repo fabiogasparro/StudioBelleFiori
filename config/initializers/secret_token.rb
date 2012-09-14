b74729c1f9f3e58a6703f34ecba33bf4efbe2645
Fabio Iodice Gasparro <fabio.gasparro@gmail.com>
2012-09-14 20:48:45 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudioBelleFiori::Application.config.secret_token = '2ae541517f914b97e589f5a8c0548644d4db94f088262215cc83890b1e58e6231fdfd7ff49221568afee613a7d4e5808bf36de8dfada3bb46c99727ebfef5c6c'
